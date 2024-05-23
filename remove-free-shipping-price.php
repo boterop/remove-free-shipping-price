bee4ea5ab61db8a3465408e1d9cb837cdcf948aa
boterop <boterop22@gmail.com>
2024-05-23 16:16:18 -0500

    Strong text

--- remove-free-shipping-price.php
+++ remove-free-shipping-price.php
@@ -12,13 +12,18 @@
 
 function change_shipping_script()
 {
-    $free = "Gratis!";
+    $free = "<strong>Gratis!</strong>";
     return 'document.addEventListener("DOMContentLoaded", () => {
-        let cartTotals = document.getElementsByClassName("cart_totals ").item(0);
-        let shippingTotals = cartTotals.getElementsByClassName("shipping ").item(0);
-        let calculateShippingText = shippingTotals.children[1].children[0];
-        calculateShippingText.innerHTML = "' . $free . '";
-    });';
+        try {
+          let cartTotals = document.getElementsByClassName("cart_totals ").item(0);
+          let shippingTotals = cartTotals.getElementsByClassName("shipping ").item(0);
+          let calculateShippingText = shippingTotals.children[1].children[0];
+          calculateShippingText.innerHTML = "' . $free . '";
+        } catch (error) {
+          console.log("Error price: ");
+        }
+      });
+      ';
 }
 
 function remove_price()
