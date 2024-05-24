45ae4cbdd7c15e486d5ebc947a1c5601f14d0c37
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-05-24 08:50:24 -0500

    Get free amount from zones (#6)
    * Get free min value
    
    * Geo locate client

--- remove-free-shipping-price.php
+++ remove-free-shipping-price.php
@@ -12,35 +12,77 @@
 
 function change_shipping_script()
 {
-    $free = "<strong>Gratis!</strong>";
-    return 'document.addEventListener("DOMContentLoaded", () => {
+  $free = "<strong>Gratis!</strong>";
+  return 'document.addEventListener("DOMContentLoaded", () => {
         try {
           let cartTotals = document.getElementsByClassName("cart_totals ").item(0);
           let shippingTotals = cartTotals.getElementsByClassName("shipping ").item(0);
           let calculateShippingText = shippingTotals.children[1].children[0];
           calculateShippingText.innerHTML = "' . $free . '";
         } catch (error) {
-          console.log("Error price: ");
+          console.log("Shipping price not found.");
         }
       });
       ';
 }
 
+function get_user_geo_country()
+{
+  $geo = new WC_Geolocation();
+  $user_ip = $geo->get_ip_address();
+  $user_geo = $geo->geolocate_ip($user_ip);
+  $country_code = $user_geo['country'];
+  return WC()->countries->countries[$country_code];
+}
+
+function get_free_shipping_min_value($zone_name = 'Colombia')
+{
+  $result = 75000;
+  $zone = null;
+
+  $zones = WC_Shipping_Zones::get_zones();
+  foreach ($zones as $z) {
+    if ($z['zone_name'] == $zone_name) {
+      $zone = $z;
+      break;
+    }
+  }
+
+  if ($zone) {
+    $shipping_methods_nl = $zone['shipping_methods'];
+    $free_shipping_method = false;
+    foreach ($shipping_methods_nl as $method) {
+      if ($method->id == 'free_shipping') {
+        $free_shipping_method = $method;
+        break;
+      }
+    }
+
+    if ($free_shipping_method) {
+      $result = $free_shipping_method->min_amount;
+    }
+  }
+
+  return $result;
+}
+
 function remove_price()
 {
-    global $woocommerce;
-    $order_total = $woocommerce->cart->get_subtotal();
-    if ($order_total >= 75000) {
-        echo "<script>console.log('Removing shipping price...' );</script>";
-        echo "<script>" . change_shipping_script() . "</script>";
-    }
+  global $woocommerce;
+  $order_total = $woocommerce->cart->get_subtotal();
+  $user_geo = get_user_geo_country();
+  $free_shipping = get_free_shipping_min_value($user_geo);
+  if ($order_total >= $free_shipping) {
+    echo "<script>console.log('Removing shipping price...' );</script>";
+    echo "<script>" . change_shipping_script() . "</script>";
+  }
 }
 
 function get_cart_item_data($data, $cartItem)
 {
-    remove_price();
+  remove_price();
 
-    return $data;
+  return $data;
 }
 
 add_filter('woocommerce_get_item_data', 'get_cart_item_data', 10, 2);
